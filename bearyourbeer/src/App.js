e320d770f6f517e3da118d6966c091599ccf548e
Olivier.Sohy <gorgobox@gmail.com>
2018-11-08 10:37:04 +0100

    first_page

--- bearyourbeer/src/App.js
+++ bearyourbeer/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import First_page from './components/first_page/first_page'
+import Footer from './components/Footer/Footer'
+
 
 class App extends Component {
   render() {
     return (
       
       <First_page />
+      <Footer />
 
     );
   }
