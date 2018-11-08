1f334cb7749ae6817e59f3c5daaf509004ae3280
PradalCyril <pradal.cyril@gmail.com>
2018-11-08 10:30:01 +0100

    problem heigth fixed

--- bearyourbeer/src/components/Footer/Footer.js
+++ bearyourbeer/src/components/Footer/Footer.js
@@ -12,16 +12,16 @@ class Footer extends Component {
                     <div><h5 className="footer-text"> © 2018 Bear Your Beer</h5></div>
                     <div className="social-like">
                         <div className="fb-like">
-                            <a href="https://www.facebook.com/wildcodeschool/" class="sharebox">
-                                <i class="fab fa-facebook-f"></i>
-                                <span class="fb-icon"></span>
+                            <a href="https://www.facebook.com/wildcodeschool/" className="sharebox">
+                                <i className="fab fa-facebook-f"></i>
+                                <span className="fb-icon"></span>
                             </a>
                         </div>
 
                         <div className='fb-like tw-like'>
-                            <a href="https://twitter.com/WildSchool_Bxl" class="sharebox">
-                                <i class="fab fa-twitter"></i>
-                                <span class="fb-icon"></span>
+                            <a href="https://twitter.com/WildSchool_Bxl" className="sharebox">
+                                <i className="fab fa-twitter"></i>
+                                <span className="fb-icon"></span>
                             </a>
                         </div>
                     </div>
