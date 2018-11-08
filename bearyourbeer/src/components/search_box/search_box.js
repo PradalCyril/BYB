930be7dca1f8d4e91159810bee91c06e4ef2ecff
Olivier.Sohy <gorgobox@gmail.com>
2018-11-08 15:27:50 +0100

    Add search-box autocompleted dependecies + search_box component

--- bearyourbeer/src/components/search_box/search_box.js
+++ bearyourbeer/src/components/search_box/search_box.js
@@ -1,24 +1,74 @@
 import React, { Component } from 'react';
 import './search_box.css';
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+  } from 'react-places-autocomplete';
+
+// API Google Map : AIzaSyCPzxx1Hx18ZT4q2ONjkyFWYRVhlmNrN-I
 
 
 
-class Search_box extends Component {
-    render() {
-        return (
-
-            <div>
-                        <form method="post" action="traitement.php">
-                            <p>
-                                <label className="font_txt" for="adresse"></label><input className="custom_input" placeholder="I choose the address of the tour" type="text" name="address" id="address" />
-                            </p>
-                        </form>
-                 
-            </div>
-
-        )
+class Search_box extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { address: '' };
     }
-}
+  
+    handleChange = address => {
+      this.setState({ address });
+    };
+  
+    handleSelect = address => {
+      geocodeByAddress(address)
+        .then(results => getLatLng(results[0]))
+        .then(latLng => console.log('Success', latLng))
+        .catch(error => console.error('Error', error));
+    };
+  
+    render() {
+      return (
+        <PlacesAutocomplete
+          value={this.state.address}
+          onChange={this.handleChange}
+          onSelect={this.handleSelect}
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+            <div>
+              <input
+                {...getInputProps({
+                  placeholder: 'Search Places ...',
+                  className: 'location-search-input',
+                })}
+              />
+              <div className="autocomplete-dropdown-container">
+                {loading && <div>Loading...</div>}
+                {suggestions.map(suggestion => {
+                  const className = suggestion.active
+                    ? 'suggestion-item--active'
+                    : 'suggestion-item';
+                  // inline style for demonstration purpose
+                  const style = suggestion.active
+                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                  return (
+                    <div
+                      {...getSuggestionItemProps(suggestion, {
+                        className,
+                        style,
+                      })}
+                    >
+                      <span>{suggestion.description}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </PlacesAutocomplete>
+      );
+    }
+  }
 
 
 export default Search_box;
