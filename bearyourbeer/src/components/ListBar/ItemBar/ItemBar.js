83baa4853d754b98ad1f8d567684516f03fcd59f
Thomas Lambert <lambertthomas10@gmail.com>
2018-11-14 13:56:16 +0100

    start working on list of bars

--- bearyourbeer/src/components/ListBar/ItemBar/ItemBar.js
+++ bearyourbeer/src/components/ListBar/ItemBar/ItemBar.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
+import React from "react";
 import './ItemBar.css';
 
-
-class ItemBar extends Component {
-    render() {
-        return (
+const ItemBar = ({photo, title, adress, description, note}) => (
             <li className="item-bar">
                 <div className="item-bar-container">
                     <div className="img-bar-container">
-                        <img className="img-bar" src={ require('../images/bar.jpg') } alt="img" />
+                        <img className="img-bar" src={photo} alt={title} />
                     </div>
                     <div className="text-bar-container">
-                        <h3 className="title-bar">Les champs Elysée <span className="point-bar">7,5</span></h3>
-                        <p className="adresse-bar">(1000 Paris)</p>
-                        <p>Bar avec plus de 2000 bières. Concert le jeudi soir et objets déco sur l'univers de la brasserie.</p>
+                        <h3 className="title-bar">{title}<span className="point-bar">{note}</span></h3>
+                        <p className="adresse-bar">{adress}</p>
+                        <p>{description}</p>
                     </div>
                 </div>
             </li>
-        )
-    }
-}
-
+        );
 
 export default ItemBar;
--- bearyourbeer/src/components/ListBar/ListBar.js
+++ bearyourbeer/src/components/ListBar/ListBar.js
@@ -1,24 +1,45 @@
-import React, { Component } from 'react';
-import './ListBar.css';
-import ItemBar from './ItemBar/ItemBar'
+import React from "react";
+import "./ListBar.css"
+import ItemBar from "./ItemBar/ItemBar";
 
-class ListBar extends Component {
-    render() {
-        return (
-            <div className="container list-bar-container">
-                <div>
-                    <span>We found 6 bars you could be interested in</span>
-                </div>
-                
-                <div>
-                    <ul className="ub-list-bar">
-                        <ItemBar />
-                    </ul>
-                </div>
-            </div>
-        )
-    }
-}
+// src/Quotes.js
+const bars = [
+  {
+    title:"Test 1",
+    adress: "5000 Namur",
+    photo:"https://u.tfstatic.com/restaurant_photos/581/201581/169/612/kombi-bar-vue-de-la-salle-87e4b.jpg",
+    description: "Bar avec plus de 2000 bières. Concert le jeudi soir et objets déco sur l'univers de la brasserie.",
+    note: "7,4"
+  },
+  {
+    title:"Test 2",
+    adress: "7510 bx",
+    photo:"https://u.tfstatic.com/restaurant_photos/581/201581/169/612/kombi-bar-vue-de-la-salle-87e4b.jpg",
+    description: "Bar avec plus de 2000 bières. Concert le jeudi soir et objets déco sur l'univers de la brasserie.",
+    note: "7,4"
+  },
+  {
+    title:"Test 3",
+    adress: "5040 Paris",
+    photo:"https://u.tfstatic.com/restaurant_photos/581/201581/169/612/kombi-bar-vue-de-la-salle-87e4b.jpg",
+    description: "Bar avec plus de 2000 bières. Concert le jeudi soir et objets déco sur l'univers de la brasserie.",
+    note: "7,4"
+  },
+  {
+    title:"Test 4",
+    adress: "54556 Londre",
+    photo:"https://u.tfstatic.com/restaurant_photos/581/201581/169/612/kombi-bar-vue-de-la-salle-87e4b.jpg",
+    description: "Bar avec plus de 2000 bières. Concert le jeudi soir et objets déco sur l'univers de la brasserie.",
+    note: "7,4"
+  }
+];
 
+const ListBar = () => (
+  <div>
+    {bars.map((propri, index) => (
+      <ItemBar key={index} title={propri.title} photo={propri.photo} adress={propri.adress} description={propri.description} note={propri.note}  />
+    ))}
+    </div>
+);
 
-export default ListBar ;
+export default ListBar;
