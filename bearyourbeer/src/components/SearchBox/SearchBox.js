e9efa9b38c38b8266c22f59795a7f7b472c2817b
Olivier.Sohy <gorgobox@gmail.com>
2018-11-14 16:12:00 +0100

    Add a return under latLng

--- bearyourbeer/src/components/SearchBox/SearchBox.js
+++ bearyourbeer/src/components/SearchBox/SearchBox.js
@@ -21,8 +21,14 @@ class SearchBox extends Component {
       geocodeByAddress(address)
         .then(results => getLatLng(results[0]))
         .then(latLng => console.log('Success', latLng))
-        .catch(error => console.error('Error', error));
+        .then(latLng => {
+          return {
+            latLng
+          };
+        });
     };
+
+    
     
   
     render() {
