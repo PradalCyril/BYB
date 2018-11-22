265b29d5783578bd34e7dd6242e916bde884dea0
PradalCyril <pradal.cyril@gmail.com>
2018-11-22 10:31:51 +0100

    tentative de recuperer la liste des bars

---
+++ bearyourbeer/src/components/Bar_on_map/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class BarsOnMap extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                lat: '50.8475967',
+                lng: '4.3586516'
+            },
+            userdistance:'1000',
+            geometry: []
+        }
+    }
+    
+    render() {
+        console.log(this.state.geometry)
+        return (
+            
+            <div>
+            <button onClick={() => this.getBars}></button>
+
+
+            </div>
+        )
+    }
+    getBars() {
+        fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=50.8475967,4.3586516&radius=1000&type=bar&rankby&key=AIzaSyCPzxx1Hx18ZT4q2ONjkyFWYRVhlmNrN-I`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    geometry: data.results.geometry.location
+                });
+            }
+            )
+    }
+}
+
+export default BarsOnMap;
