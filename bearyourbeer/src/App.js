170d5a15265097f0b6a019b385ae56700cd0e758
PradalCyril <pradal.cyril@gmail.com>
2018-11-26 16:36:19 +0100

    ajout lifting state up

--- bearyourbeer/src/App.js
+++ bearyourbeer/src/App.js
@@ -10,13 +10,23 @@ import BarOnMap from './components/Bar_on_map/index'
 class App extends Component {
   constructor(props) {
     super(props);
-    this.handBeerDistance=this.handBeerDistance.bind(this);
-    this.handDistance=this.handDistance.bind(this);
-    this.handNbBar=this.handNbBar.bind(this);
+    this.handBeerDistance = this.handBeerDistance.bind(this);
+    this.handDistance = this.handDistance.bind(this);
+    this.handNbBar = this.handNbBar.bind(this);
     this.state = {
       uservalue1: 1,
       uservalue2: 2,
-      uservalue3: 3
+      uservalue3: 3,
+      bar1: [],
+      bar2: [],
+      bar3: [],
+      bar4: [],
+      bar5: [],
+      bar6: [],
+      bar7: [],
+      bar8: [],
+      bar9: [],
+      bar10: []
     }
   }
 
@@ -30,16 +40,59 @@ class App extends Component {
   handNbBar(user) {
     this.setState({ uservalue3: user });
   }
-  
+  getFirstBar(bar){
+    this.setState({bar1: bar})
+  }
+  getSecondBar(bar){
+    this.setState({bar2: bar})
+  }
+  getThirdBar(bar){
+    this.setState({bar3: bar})
+  }
+  getForthBar(bar){
+    this.setState({bar4: bar})
+  }
+  getFifthBar(bar){
+    this.setState({bar5: bar})
+  }
+  getSixthBar(bar){
+    this.setState({bar6: bar})
+  }
+  getSeventhBar(bar){
+    this.setState({bar7: bar})
+  }
+  getEighthBar(bar){
+    this.setState({bar8: bar})
+  }
+  getNinthBar(bar){
+    this.setState({bar9: bar})
+  }
+  getTenthBar(bar){
+    this.setState({bar10: bar})
+  }
+componentDidUpdate(){
+  console.log(this.state.bar1)
+}
   render() {
     return (
       <div>
-      <Range_slider
-        onChangeValue1={this.handBeerDistance}
-        onChangeValue2={this.handDistance}
-        onChangeValue3={this.handNbBar}
-      />
-      <BarOnMap />
+        <Range_slider
+          onChangeValue1={this.handBeerDistance}
+          onChangeValue2={this.handDistance}
+          onChangeValue3={this.handNbBar}
+        />
+        <BarOnMap 
+          getBar1={(bar) => getFirstBar(bar)}
+          getBar2={(bar) => getSecondBar(bar)}
+          getBar3={(bar) => getThirdBar(bar)}
+          getBar4={(bar) => getForthBar(bar)}
+          getBar5={(bar) => getFiveBar(bar)}
+          getBar6={(bar) => getSexthBar(bar)}
+          getBar7={(bar) => getSeventhBar(bar)}
+          getBar8={(bar) => getEightBar(bar)}
+          getBar9={(bar) => getNinthBar(bar)}
+          getBar10={(bar) => getTenthBar(bar)}
+          />
       </div>
     );
   }
--- bearyourbeer/src/components/Bar_on_map/index.js
+++ bearyourbeer/src/components/Bar_on_map/index.js
@@ -9,18 +9,14 @@ class BarsOnMap extends Component {
                 lng: '4.3586516'
             },
             userdistance: '1000',
-            bar1: [],
-            bar2: [],
-            bar3: [],
-            bar4: [],
-            bar5: []
+
         }
     }
 
     render() {
 
         return (
-            <div>    
+            <div>
             </div>
         )
     }
@@ -35,20 +31,31 @@ class BarsOnMap extends Component {
             })
             .then(response => response.json())
             .then((data1) => {
-                this.setState({ bar1: data1.results[0], bar2: data1.results[1], bar3: data1.results[2], bar4: data1.results[3], bar5: data1.results[4] });
-            })
-    }
-    componentDidUpdate() {
-        console.log(this.state.user.lat)
-        console.log(this.state.userdistance)
-            console.log(this.state.bar2.geometry.location)
-            console.log(this.state.bar3.geometry.location)
-            console.log(this.state.bar4.geometry.location)
-            console.log(this.state.bar5.geometry.location)
-        
-        
+                this.props.getBar1(data1.results[0]),
+                this.props.getBar2(data1.results[1]),
+                this.props.getBar3(data1.results[2]),
+                this.props.getBar4(data1.results[3]),
+                this.props.getBar5(data1.results[4]),
+                this.props.getBar6(data1.results[5]),
+                this.props.getBar7(data1.results[6]),
+                this.props.getBar8(data1.results[7]),
+                this.props.getBar9(data1.results[8]),
+                this.props.getBar10(data1.results[9])
 
-    }
+            });
+    
+}
+componentDidUpdate() {
+    console.log(this.state.user.lat)
+    console.log(this.state.userdistance)
+    console.log(this.state.bar2.geometry.location)
+    console.log(this.state.bar3.geometry.location)
+    console.log(this.state.bar4.geometry.location)
+    console.log(this.state.bar5.geometry.location)
+
+
+
+}
 }
 
 
