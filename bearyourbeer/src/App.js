999c19d7a61e77733ca9a033df8a988572180640
cormobro <bonaet.felix@gmail.com>
2018-11-29 11:02:34 +0100

    correction routing

--- bearyourbeer/src/App.js
+++ bearyourbeer/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import FirstPage from './components/FirstPage/FirstPage';
 import {Switch, Route} from 'react-router-dom';
-import Geopage from './Geo-page';
-import Addresspage from './Address-page';
+import Geopage from './geopage';
+import Addresspage from './addresspage';
 import SearchBox from './components/SearchBox/SearchBox'
 import FirstPage from './components/FirstPage/FirstPage'
 
@@ -13,8 +13,8 @@ class App extends Component {
       <div>
 	    <Switch>
 		<Route exact path="/" component={FirstPage} />
-                  <Route path="/geo-page" render={props => <Geopage location={this.state.bars} />} />
-                  <Route path="/address-page" render={props => <Addresspage location={this.state.bars} addresslocation={this.state.data} />} />
+                  <Route path="/geopage" render={props => <Geopage location={this.state.bars} />} />
+                  <Route path="/addresspage" render={props => <Addresspage location={this.state.bars} addresslocation={this.state.data} />} />
 	    </Switch>
       </div>
     );
