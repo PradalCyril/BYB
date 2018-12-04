47fd979bebf5075b97c2b380dd48d3f1049833e8
cormobro <bonaet.felix@gmail.com>
2018-12-04 13:40:00 +0100

    modifs carte

--- bearyourbeer/src/components/Geomap/SimpleMap.js
+++ bearyourbeer/src/components/Geomap/SimpleMap.js
@@ -4,17 +4,17 @@ import {  getLocation } from './API';
 import ouvrebouteille from './ouvrebouteille2.svg';
 
 
-const AnyReactComponent = ({ image }) => <div><img src={ouvrebouteille} alt="location pin" height="42" width="42" /></div>;
-const AnyReactComponent1 = ({ image }) => <div>{this.state.pins[0]}</div>;
-const AnyReactComponent2 = ({ image }) => <div>{this.state.pins[1]}</div>;
-const AnyReactComponent3 = ({ image }) => <div>{this.state.pins[2]}</div>;
-const AnyReactComponent4 = ({ image }) => <div>{this.state.pins[3]}</div>;
-const AnyReactComponent5 = ({ image }) => <div>{this.state.pins[4]}</div>;
-const AnyReactComponent6 = ({ image }) => <div>{this.state.pins[5]}</div>;
-const AnyReactComponent7 = ({ image }) => <div>{this.state.pins[6]}</div>;
-const AnyReactComponent8 = ({ image }) => <div>{this.state.pins[7]}</div>;
-const AnyReactComponent9 = ({ image }) => <div>{this.state.pins[8]}</div>;
-const AnyReactComponent10 = ({ image }) => <div>{this.state.pins[9]}</div>;
+const AnyReactComponent = () => <div><img src={ouvrebouteille} alt="location pin" height="42" width="42" /></div>;
+const AnyReactComponent1 = () => {this.state.pins[0]};
+const AnyReactComponent2 = () => {this.state.pins[1]};
+const AnyReactComponent3 = () => {this.state.pins[2]};
+const AnyReactComponent4 = () => {this.state.pins[3]};
+const AnyReactComponent5 = () => this.state.pins[4]};
+const AnyReactComponent6 = () => {this.state.pins[5]};
+const AnyReactComponent7 = () => {this.state.pins[6]};
+const AnyReactComponent8 = () => {this.state.pins[7]};
+const AnyReactComponent9 = () => {this.state.pins[8]};
+const AnyReactComponent10 = () => {this.state.pins[9]};
 
 class SimpleMap extends Component {
 	constructor(props){	
@@ -30,11 +30,12 @@ class SimpleMap extends Component {
 			},
 			haveUsersLocation: false,
 			zoom: 12,
-			pins: [new Array(10)]
+			pins: [new Array(10)],
+			createPins: false
 		}
 	}
 
-		componentDidMount() {
+	componentDidMount() {
 		const geolocated = props.geolocated;
 		if(geolocated){
 			getLocation()
@@ -45,39 +46,45 @@ class SimpleMap extends Component {
 						zoom: 16
 					});
 				});
-				.then(location => {
-					this.props.geocoords(location)
-				});
+			.then(location => {
+				this.props.geocoords(location)
+			});
 		} 
 
 	}
-	render() {
-		this.setState.pins.map(pin => { return (
-			<svg width="42" height="35" xmlns="http://www.w3.org/2000/svg">
-                        <g>
-                        <title>background</title>
-                        <g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">
-                        <rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/>
-                        </g>
-                        </g>
-                        <g>
-                        <title>Layer 1</title>
-                        <defs>
-                        <style>.cls-1{fill:#707070;}.cls-2{fill:none;stroke:#ffd400;stroke-miterlimit:10;stroke-width:2px;}</style>
-                        </defs>
-                        <path d="m270.49884,110.205498c0,1.500092 -0.054718,2.492767 -0.500031,6.000366c-0.570251,4.491905 -1.505463,9.134903 -3.500214,15.500938c-1.610291,5.139114 -2.000122,7.500458 -2.000122,8.000488l0,0.500031l0,1.000061" id="svg_31" stroke-opacity="null" stroke-width="0" stroke="#000" fill="none"/>
-                        <g stroke="null" id="svg_38">
-                        <g stroke="null" id="svg_36" fill="#000000" transform="matrix(0.04863362245160378,0,0,-0.03910566381637382,-1744.6474878627478,-403.10109946368186) ">
-                        <path stroke="null" fill="#da2627" id="svg_37" d="m41681.165396,-10539.970599c-276,-28 -392,-47 -590,-99c-853,-222 -1563,-798 -1958,-1590c-299,-600 -385,-1335 -232,-1975c76,-316 165,-524 413,-970c269,-484 315,-559 787,-1285c111,-170 256,-395 322,-500c768,-1214 1218,-2270 1474,-3459c15,-68 18,-62 44,64c104,524 325,1206 568,1755c302,682 612,1227 1260,2214c450,688 566,882 791,1331c253,502 304,636 370,960c69,340 65,774 -10,1150c-115,575 -417,1120 -860,1551c-471,457 -1078,743 -1774,834c-118,15 -516,28 -605,19z"/>
-                        </g>
-                        </g>
-                        <text stroke="#000" transform="matrix(13.433609008789062,0,0,10.528879165649412,-2937.271930456162,-680.633221387863) " xml:space="preserve" text-anchor="start" font-family="Georgia, Times, 'Times New Roman', serif" font-size="24" id="svg_43" y="85.15089" x="234.182731" stroke-opacity="null" stroke-width="0" fill="#ffffff">{index}</text>
-                        <defs>
-                        <style>.cls-1{fill:#da2627;}.cls-2{fill:#fff;}</style>
-                        </defs>
-                        </g>
-                        </svg>
-		)}
+
+	componentWillMount() {
+			if(this.state.createPins === false){
+				for(let i = 1; i < 11; i++){
+					this.setState.pins.push(`<svg width="42" height="35" xmlns="http://www.w3.org/2000/svg">
+						<g>
+						<title>background</title>
+						<g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">
+						<rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/>
+						</g>
+						</g>
+						<g>
+						<title>Layer 1</title>
+						<defs>
+						<style>.cls-1{fill:#707070;}.cls-2{fill:none;stroke:#ffd400;stroke-miterlimit:10;stroke-width:2px;}</style>
+						</defs>
+						<path d="m270.49884,110.205498c0,1.500092 -0.054718,2.492767 -0.500031,6.000366c-0.570251,4.491905 -1.505463,9.134903 -3.500214,15.500938c-1.610291,5.139114 -2.000122,7.500458 -2.000122,8.000488l0,0.500031l0,1.000061" id="svg_31" stroke-opacity="null" stroke-width="0" stroke="#000" fill="none"/>
+						<g stroke="null" id="svg_38">
+						<g stroke="null" id="svg_36" fill="#000000" transform="matrix(0.04863362245160378,0,0,-0.03910566381637382,-1744.6474878627478,-403.10109946368186) ">
+						<path stroke="null" fill="#da2627" id="svg_37" d="m41681.165396,-10539.970599c-276,-28 -392,-47 -590,-99c-853,-222 -1563,-798 -1958,-1590c-299,-600 -385,-1335 -232,-1975c76,-316 165,-524 413,-970c269,-484 315,-559 787,-1285c111,-170 256,-395 322,-500c768,-1214 1218,-2270 1474,-3459c15,-68 18,-62 44,64c104,524 325,1206 568,1755c302,682 612,1227 1260,2214c450,688 566,882 791,1331c253,502 304,636 370,960c69,340 65,774 -10,1150c-115,575 -417,1120 -860,1551c-471,457 -1078,743 -1774,834c-118,15 -516,28 -605,19z"/>
+						</g>
+						</g>
+						<text stroke="#000" transform="matrix(13.433609008789062,0,0,10.528879165649412,-2937.271930456162,-680.633221387863) " xml:space="preserve" text-anchor="start" font-family="Georgia, Times, 'Times New Roman', serif" font-size="24" id="svg_43" y="85.15089" x="234.182731" stroke-opacity="null" stroke-width="0" fill="#ffffff">` + i +`</text>
+						<defs>
+						<style>.cls-1{fill:#da2627;}.cls-2{fill:#fff;}</style>
+						</defs>
+						</g>
+						</svg>`
+					)}
+				this.setState({ createPins: true})
+			}
+	}
+	render(){
 		const position = [this.state.location.lat, this.state.location.lng];
 		return (
 			<div style={{ height: '100vh', width: '100%' }}>
@@ -91,10 +98,10 @@ class SimpleMap extends Component {
 			lng={this.state.location.lng}
 			/>
 			<AnyReactComponent1
-			lat={this.state.location.lat}
-			lng={this.state.location.lng}
-			>
-			/*	<AnyReactComponent2
+			lat={this.props.location[0].geometry.location.lat}
+			lng={this.props.location[0].geometry.location.lng}
+			/>
+			<AnyReactComponent2
 			lat={this.props.location[1].geometry.location.lat}
 			lng={this.props.location[1].geometry.location.lng}
 
@@ -137,7 +144,7 @@ class SimpleMap extends Component {
 			<AnyReactComponent10
 			lat={this.props.location[9].geometry.location.lat}
 			lng={this.props.location[9].geometry.location.lng}
-			/> */
+			/>
 			</GoogleMapReact>
 			</div>
 		);
